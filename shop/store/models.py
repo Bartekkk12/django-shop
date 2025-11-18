d40976d71bff4e5bed365d0d220e9482f9fc0d06
Bartekkk12 <pirogbartosz12@gmail.com>
2025-11-18 20:19:37 +0100

    Create, migrate and test new models (Category, Product, Review)

--- shop/store/models.py
+++ shop/store/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name).lower()
+            slug = base_slug
+            counter = 1
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    nutrition = models.TextField(blank=True)
+    ingredients = models.TextField(blank=True)
+    method_of_use = models.TextField(blank=True)
+    
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveBigIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    is_active = models.BooleanField(default=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name).lower()
+            slug = base_slug
+            counter = 1
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'slug': self.slug})
+            
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    content = models.TextField(blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.product.name} - {self.rating}/5"
--- shop/store/tests.py
+++ shop/store/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
+from .models import Product, Category, Review
+
 # Create your tests here.
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Whey Protein")
+        self.product = Product.objects.create(
+            name="Whey Protein",
+            category= self.category,
+            price = 50.99,
+            stock=15
+        )
+    
+    def test_slug_creation(self):
+        self.assertEqual(self.product.slug, 'whey-protein')
+    
+    def test_string_representation(self):
+        self.assertEqual(str(self.product), 'Whey Protein')
+        
+        
+class ReviewTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Creatine")
+        self.product = Product.objects.create(
+            name="Creatine",
+            category= self.category,
+            price = 22.99,
+            stock=15
+        )
+        self.review = Review(
+            product = self.product,
+            rating = 3,
+            content = "Amazing product!"
+        )
+        
+    def test_review_str(self):
+        self.assertEqual(str(self.review), "Creatine - 3/5")
